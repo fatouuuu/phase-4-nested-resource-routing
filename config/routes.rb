73de7a6c46a7e912c27996353216cb05252d7376
fatouuuu <fatmaabdullrahim@gmail.com>
2023-03-26 14:05:54 +0300

    completed code-along

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  resources :dog_houses, only: [:show]
+  resources :dog_houses, only: [:show] do 
+    # nest resources for reviews under dog_houses here -why? good separation of concerns and easier to read code 
+    resources :reviews, only: [:index, :show]
+  end
   resources :reviews, only: [:show, :index, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # get 'dog_houses/:dog_house_id/reviews', to: 'dog_houses#reviews_index'
+  # get 'dog_houses/:dog_house_id/reviews/:id', to: 'dog_houses#review'
 end
